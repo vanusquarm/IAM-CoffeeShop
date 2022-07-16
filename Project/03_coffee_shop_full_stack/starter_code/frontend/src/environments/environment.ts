f4bb46fbccd885d13c22ec0ad52c98de9df472ef
vanusquarm <vanusquarm@gmail.com>
2022-07-16 13:12:45 +0000

    Set configuration arguments for auth0

--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
-  }
+    url: 'https://dev-9ogbv8za.us', // the auth0 domain prefix
+    audience: 'image', // the audience set for the auth0 app
+    clientId: '0XvcVdgARuVeSdGM5lEdxRaBJ81mVLB0', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
+  },
 };
